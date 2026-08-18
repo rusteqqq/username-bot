f48406e117401617561c2e314eb1a0446803d05c
rusteqqq <blydorez@gmail.com>
2026-08-18 16:00:45 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,18 @@
 BOT_TOKEN = "8985136222:AAGO0DqsK-vqTJ4oCOgPhtkKaWbJmOMmdB8"
 
+# API ID и API HASH: https://my.telegram.org -> API development tools
 API_ID = 23814953
 API_HASH = "7f2e82275c8c00777a6ee7955e0810a9"
 
+# Telegram-сессии
 SESSION_NAMES = [
     "scanner_1",
     "scanner_2",
     "scanner_3",
 ]
 
-DATABASE_PATH = "usernames.db"
+# Пауза между проверками
+CHECK_DELAY = 2
 
-CHECK_DELAY = 2.0
+# База данных
+DATABASE_PATH = "bot.db"
