76e003934e0ce54f30ca5b9e1d7af4bd2c3396ad
rusteqqq <blydorez@gmail.com>
2026-08-18 10:09:10 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,9 @@
-BOT_TOKEN = "8985136222:AAGO0DqsK-vqTJ4oCOgPhtkKaWbJmOMmdB8"
+SESSION_NAMES = [
+    "scanner_1",
+    "scanner_2",
+    "scanner_3",
+]
 
-# API ID и API HASH: https://my.telegram.org -> API development tools
-API_ID = 23814953
-API_HASH = "7f2e82275c8c00777a6ee7955e0810a9"
-
-SESSION_NAME = "username_scanner"
 DATABASE_PATH = "usernames.db"
 
-# Пауза между проверками. Telegram сам может применить FloodWait.
-# Не пытайся обходить лимиты большим количеством аккаунтов/прокси.
-CHECK_DELAY = 0.15
+CHECK_DELAY = 2.0
