5a9a2a6bf54a7c0e8f9d77617a739fc69ff0b924
rusteqqq <blydorez@gmail.com>
2026-08-18 10:12:15 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+BOT_TOKEN = "8985136222:AAGO0DqsK-vqTJ4oCOgPhtkKaWbJmOMmdB8"
+
+API_ID = 23814953
+API_HASH = "7f2e82275c8c00777a6ee7955e0810a9"
+
 SESSION_NAMES = [
     "scanner_1",
     "scanner_2",
